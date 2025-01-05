5199d3ff5870d38aaa51303d8c82376a4de3d625
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2025-01-05 13:12:27 +0530

    Add second-highest-salary.cpp

---
+++ second-highest-salary.cpp
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    IF(
+        (SELECT COUNT(DISTINCT salary) FROM Employee) < 2, 
+        NULL, 
+        (SELECT DISTINCT salary 
+         FROM Employee 
+         ORDER BY salary DESC 
+         LIMIT 1 OFFSET 1)
+    ) AS SecondHighestSalary;
+
